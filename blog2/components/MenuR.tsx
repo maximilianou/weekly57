39a4ce0f53ec0061c53e9c7ab0c18d7f6b87aa0b
maximilianou <maximilianou@gmail.com>
2022-07-07 17:54:26 +0200

    feat(ui): css styling react extract typescript values data

--- blog2/components/MenuR.tsx
+++ blog2/components/MenuR.tsx
@@ -1,31 +1,37 @@
 
 const MenuR = () => {
+
+  const menuStyle = `fixed w-[100px] top-20
+    transition-all -right-20  hover:right-0
+  `;
+  const manuItemStyle = `p-4 
+    inline-block w-full
+    rounded-xl bg-[rgb(100,100,100,0.5)] `;
+
+  const menuItemData: { link:string , text:string }[] = [
+    {
+      link: `#`,
+      text: `About`,
+    },
+    {
+      link: `#`,
+      text: `Blog`,
+    },
+    {
+      link: `#`,
+      text: `Projects`,
+    },
+    {
+      link: `#`,
+      text: `Contact`,
+    },
+  ]
+
   return (
-  <div >
-    <a href="#" className={` 
-    fixed top-20 w-[100px] 
-    transition-opacity p-4  
-    rounded-md -right-20  hover:right-0 
-    bg-[rgb(100,100,100,0.5)]
-    `} >About</a>
-    <a href="#" className={`
-    fixed top-40 w-[100px]
-    transition-duration-[0.3s] p-4  
-    rounded-md -right-20  hover:right-0
-    bg-[rgb(100,100,100,0.5)]
-    `}>Blog</a>
-    <a href="#" className={`
-    fixed top-60 w-[100px]
-    transition-duration-[0.3s] p-4  
-    rounded-md -right-20  hover:right-0
-    bg-[rgb(100,100,100,0.5)]    
-    `}>Projects</a>
-    <a href="#" className={`
-    fixed top-80 w-[100px]
-    transition-duration-[0.3s] p-4  
-    rounded-md -right-20  hover:right-0
-    bg-[rgb(100,100,100,0.5)]
-    `}>Contact</a>
+  <div className={menuStyle} >
+    { menuItemData && menuItemData.map( ({ link, text }, indx) => (
+    <a href={link} className={`${manuItemStyle}  `} >{text}</a>
+    ))}
   </div>
   )
 };
